ac736bc208168c01603e50874edbbbf16473c264
Robert <robert.sitek16@gmail.com>
2022-12-16 18:06:02 +0100

    Validate Products

--- src/app/modules/admin/admin-product-form/admin-product-form.component.ts
+++ src/app/modules/admin/admin-product-form/admin-product-form.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from "@angular/core";
 import { FormGroup } from "@angular/forms";
+import { reduce } from "rxjs";
 
 @Component({
     selector: 'app-admin-product-form',
@@ -8,27 +9,69 @@ import { FormGroup } from "@angular/forms";
         <mat-form-field appearance="fill">
             <mat-label>Name</mat-label>
             <input matInput placeholder="Enter Product Name" formControlName="name">
+            <div *ngIf="name?.invalid && (name?.dirty || name?.touched)" class="errorMessages">
+               <div *ngIf="name?.errors?.['required']">
+                    Name is required
+               </div>
+               <div *ngIf="name?.errors?.['minlength']">
+                    The field must have a minimum of 4 characters
+               </div>
+            </div>
         </mat-form-field>
         <mat-form-field appearance="fill">
             <mat-label>Description</mat-label>
             <textarea matInput rows="20" placeholder="Enter product description" formControlName="description"></textarea>
+            <div *ngIf="description?.invalid && (description?.dirty || description?.touched)" class="errorMessages">
+               <div *ngIf="description?.errors?.['required']">
+                    Description is required
+               </div>
+               <div *ngIf="description?.errors?.['minlength']">
+                    Description must have a minimum of 4 characters
+               </div>
+            </div>
         </mat-form-field>
         <mat-form-field appearance="fill">
             <mat-label>Category</mat-label>
             <input matInput placeholder="Enter category" formControlName="category">
+            <div *ngIf="category?.invalid && (category?.dirty || category?.touched)" class="errorMessages">
+               <div *ngIf="category?.errors?.['required']">
+                    Category is required
+               </div>
+               <div *ngIf="category?.errors?.['minlength']">
+                    Category must have a minimum of 4 characters
+               </div>
+            </div>
         </mat-form-field>
         <mat-form-field appearance="fill">
             <mat-label>Price</mat-label>
             <input matInput placeholder="Enter price" formControlName="price">
+            <div *ngIf="price?.invalid && (price?.dirty || price?.touched)" class="errorMessages">
+               <div *ngIf="price?.errors?.['required']">
+                    Price is required
+               </div>
+               <div *ngIf="price?.errors?.['min']">
+                    Price must be greater than zero
+               </div>
+            </div>
         </mat-form-field>
         <mat-form-field appearance="fill">
             <mat-label>Currency</mat-label>
             <input matInput placeholder="Enter price" value="PLN" formControlName="currency">
+            <div *ngIf="currency?.invalid && (currency?.dirty || currency?.touched)" class="errorMessages">
+               <div *ngIf="currency?.errors?.['required']">
+                    Currency is required
+               </div>
+            </div>
         </mat-form-field>
         <div fxLayout="colum" fxFlexAlign="end" fxLayoutGap="10">
-            <button mat-flat-button color="primary">Save</button>
+            <button mat-flat-button color="primary" [disabled]="!parentForm.valid">Save</button>
         </div>
-</div>`
+</div>`,
+    styles: [`
+        .errorMessages{
+            color: red;
+        }
+    `]
 })
 export class AdminProductFormComponent implements OnInit {
 
@@ -37,5 +80,26 @@ export class AdminProductFormComponent implements OnInit {
     ngOnInit(): void {
 
     }
+
+    get name() {
+        return this.parentForm.get("name");
+    }
+
+    get description() {
+        return this.parentForm.get("description");
+    }
+
+    get category() {
+        return this.parentForm.get("category");
+    }
+
+    get price() {
+        return this.parentForm.get("price");
+    }
+    
+    get currency() {
+        return this.parentForm.get("currency");
+    }
+
 
 }
--- src/app/modules/admin/admin-product-update/admin-product-update.component.ts
+++ src/app/modules/admin/admin-product-update/admin-product-update.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { ActivatedRoute } from '@angular/router';
+import { AdminMessageService } from '../admin-message.service';
 import { AdminProductUpdateService } from './admin-product-update.service';
 import { AdminProductUpdate } from './model/adminProductUpdate';
 
@@ -18,6 +19,7 @@ export class AdminProductUpdateComponent implements OnInit {
   constructor(
     private router: ActivatedRoute,
     private adminProductUpdateService: AdminProductUpdateService,
+    private adminMessageService: AdminMessageService,
     private formBuilder: FormBuilder,
     private snackBark: MatSnackBar
   ) { }
@@ -25,11 +27,11 @@ export class AdminProductUpdateComponent implements OnInit {
   ngOnInit(): void {
     this.getProduct();
     this.productForm = this.formBuilder.group({
-      name: [''],
-      description: [''],
-      category: [''],
-      price: [''],
-      currency: ['PLN']
+      name: ['', [Validators.required, Validators.minLength(4)]],
+      description: ['', [Validators.required, Validators.minLength(4)]],
+      category: ['', [Validators.required, Validators.minLength(4)]],
+      price: ['', [Validators.required, Validators.min(0)]],
+      currency: ['PLN', Validators.required]
     });
   }
 
@@ -47,9 +49,12 @@ export class AdminProductUpdateComponent implements OnInit {
       category: this.productForm.get('category')?.value,
       price: this.productForm.get('price')?.value,
       currency: this.productForm.get('currency')?.value
-    } as AdminProductUpdate).subscribe(product => {
-      this.mapFormValues(product);
-      this.snackBark.open("Product has been saved!", '', {duration: 2000});
+    } as AdminProductUpdate).subscribe({
+      next: product => {
+        this.mapFormValues(product);
+        this.snackBark.open("Product has been saved!", '', { duration: 2000 });
+      },
+      error: err => this.adminMessageService.addSpringErrors(err.error)
     });
   }
 
