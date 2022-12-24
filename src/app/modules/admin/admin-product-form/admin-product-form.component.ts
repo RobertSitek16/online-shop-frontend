9055a3d5ea09b2aa6f2f678e14d2348e37ba647f
Robert <robert.sitek16@gmail.com>
2022-12-24 10:15:54 +0100

    Category handling

--- src/app/modules/admin/admin-product-form/admin-product-form.component.ts
+++ src/app/modules/admin/admin-product-form/admin-product-form.component.ts
@@ -1,11 +1,14 @@
 import { Component, Input, OnInit } from "@angular/core";
 import { FormGroup } from "@angular/forms";
 import { reduce } from "rxjs";
+import { AdminCategoryNameDto } from "./adminCategoryNameDto";
+import { FormCategoryService } from "./form-category.service";
 
 @Component({
     selector: 'app-admin-product-form',
     template: `
     <div [formGroup]="parentForm" fxLayout="column">
+        
         <mat-form-field appearance="fill">
             <mat-label>Name</mat-label>
             <input matInput placeholder="Enter Product Name" formControlName="name">
@@ -18,6 +21,7 @@ import { reduce } from "rxjs";
                </div>
             </div>
         </mat-form-field>
+
         <mat-form-field appearance="fill">
             <mat-label>Friendly URL</mat-label>
             <input matInput placeholder="Enter Product Name" formControlName="slug">
@@ -30,6 +34,7 @@ import { reduce } from "rxjs";
                </div>
             </div>
         </mat-form-field>
+
         <mat-form-field appearance="fill">
             <mat-label>Description</mat-label>
             <textarea matInput rows="10" placeholder="Enter product description" formControlName="description"></textarea>
@@ -42,22 +47,26 @@ import { reduce } from "rxjs";
                </div>
             </div>
         </mat-form-field>
+
         <mat-form-field appearance="fill">
             <mat-label>Full Description</mat-label>
             <textarea matInput rows="30" placeholder="Enter full product description" formControlName="fullDescription"></textarea>
         </mat-form-field>
+
         <mat-form-field appearance="fill">
             <mat-label>Category</mat-label>
-            <input matInput placeholder="Enter category" formControlName="category">
-            <div *ngIf="category?.invalid && (category?.dirty || category?.touched)" class="errorMessages">
-               <div *ngIf="category?.errors?.['required']">
+            <mat-select formControlName="categoryId">
+                <mat-option *ngFor="let element of categories" [value]="element.id">
+                {{element.name}}
+                </mat-option>
+            </mat-select>
+            <div *ngIf="categoryId?.invalid && (categoryId?.dirty || categoryId?.touched)" class="errorMessages">
+               <div *ngIf="categoryId?.errors?.['required']">
                     Category is required
-               </div>
-               <div *ngIf="category?.errors?.['minlength']">
-                    Category must have a minimum of 4 characters
                </div>
             </div>
         </mat-form-field>
+        
         <mat-form-field appearance="fill">
             <mat-label>Price</mat-label>
             <input matInput placeholder="Enter price" formControlName="price">
@@ -70,6 +79,7 @@ import { reduce } from "rxjs";
                </div>
             </div>
         </mat-form-field>
+        
         <mat-form-field appearance="fill">
             <mat-label>Currency</mat-label>
             <input matInput placeholder="Enter price" value="PLN" formControlName="currency">
@@ -79,6 +89,7 @@ import { reduce } from "rxjs";
                </div>
             </div>
         </mat-form-field>
+        
         <div fxLayout="colum" fxFlexAlign="end" fxLayoutGap="10">
             <button mat-flat-button color="primary" [disabled]="!parentForm.valid">Save</button>
         </div>
@@ -92,9 +103,17 @@ import { reduce } from "rxjs";
 export class AdminProductFormComponent implements OnInit {
 
     @Input() parentForm!: FormGroup;
+    categories: Array<AdminCategoryNameDto> = [];
+
+    constructor(private formCategoryService: FormCategoryService) { }
 
     ngOnInit(): void {
+        this.getCategories();
+    }
 
+    getCategories() {
+        this.formCategoryService.getCategories()
+            .subscribe(categories => this.categories = categories);
     }
 
     get name() {
@@ -105,14 +124,14 @@ export class AdminProductFormComponent implements OnInit {
         return this.parentForm.get("description");
     }
 
-    get category() {
-        return this.parentForm.get("category");
+    get categoryId() {
+        return this.parentForm.get("categoryId");
     }
 
     get price() {
         return this.parentForm.get("price");
     }
-    
+
     get currency() {
         return this.parentForm.get("currency");
     }
