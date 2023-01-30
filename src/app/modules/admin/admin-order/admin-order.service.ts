ebf833c41e4e89d7f996362dff90d520e3401ed7
Robert <robert.sitek16@gmail.com>
2023-01-30 16:53:50 +0100

    Sales Chart

--- src/app/modules/admin/admin-order/admin-order.service.ts
+++ src/app/modules/admin/admin-order/admin-order.service.ts
@@ -8,27 +8,31 @@ import { AdminOrder } from './model/adminOrder';
   providedIn: 'root'
 })
 export class AdminOrderService {
-  
+
   constructor(private http: HttpClient) { }
-  
+
   getOrders(pageIndex: number, pageSize: number): Observable<Page<AdminOrder>> {
     return this.http.get<Page<AdminOrder>>(`/api/admin/orders?page=${pageIndex}&size=${pageSize}`);
   }
-  
+
   getOrder(id: number): Observable<AdminOrder> {
     return this.http.get<AdminOrder>("/api/admin/orders/" + id);
   }
-  
+
   saveStatus(id: number, value: any): Observable<void> {
     return this.http.patch<void>("/api/admin/orders/" + id, value);
   }
-  
+
   getInitData(): Observable<any> {
     return this.http.get<any>("/api/admin/orders/initData");
   }
-  
+
   exportOrders(from: string, to: string, orderStatus: string): Observable<any> {
-    return this.http.get(`/api/admin/orders/export?from=${from}&to=${to}&orderStatus=${orderStatus}`, {responseType: "blob", observe: "response"});
+    return this.http.get(`/api/admin/orders/export?from=${from}&to=${to}&orderStatus=${orderStatus}`, { responseType: "blob", observe: "response" });
+  }
+
+  getSalesStatistics(): Observable<any> {
+    return this.http.get("api/admin/orders/stats");
   }
 
 }
