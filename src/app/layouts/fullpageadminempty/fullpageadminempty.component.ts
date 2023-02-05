c2f2bf260bbe7e5435ededb40c872809547b4eb3
Robert <robert.sitek16@gmail.com>
2023-02-05 15:32:51 +0100

    Admin User Login

---
+++ src/app/layouts/fullpageadminempty/fullpageadminempty.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-fullpageadmin',
+  templateUrl: './fullpageadminempty.component.html',
+  styleUrls: ['./fullpageadminempty.component.scss']
+})
+export class FullpageadminemptyComponent implements OnInit {
+
+  title = "Shop panel"
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
